9fcf56977cf8411b21f92f24f1b314c4146870bd
Fabien Pinckaers <fp@openerp.com>
2022-05-29 22:14:14 +0200

    imp

--- school_lunch/static/src/js/school_lunch.js
+++ school_lunch/static/src/js/school_lunch.js
@@ -1,5 +1,6 @@
 /** @odoo-module **/
 
+
 var ajax = require('web.ajax');
 var core = require('web.core');
 
@@ -11,6 +12,30 @@ const {Router, RouteComponent} = owl.router;
 const {whenReady} = owl.utils;
 
 class LunchMenu extends Component {
+    toggleMenu() {
+        const kid = this.props.kid.id;
+        const isset = this.props.menu.kids.includes(kid);
+        for (var meal of this.props.meals) {
+            var index = meal.kids.indexOf(kid);
+            if (index > -1)
+                delete meal.kids[index];
+        };
+        if (! isset)
+            this.props.menu.kids.push(kid);
+    };
+    static props = ["menu", "kid", "meals"];
+}
+LunchMenu.template = "school_lunch.lunch_menu"
+
+
+class LunchLine extends Component {
+    static props = ["menu", "kids"];
+    static components = { LunchMenu };
+}
+LunchLine.template = "school_lunch.lunch_line"
+
+
+class LunchMenuTable extends Component {
     setup() {
         this.state = useState({ value: 1 });
         this.kids = [
@@ -21,19 +46,22 @@ class LunchMenu extends Component {
             {id: 1, name: "Lentils"},
             {id: 2, name: "Apple"}
         ]
-        this.menus = [
-            {date: '2012-01-01'}
-
-
-
-        ]
+        this.menus = useState([
+            {
+                date: 'Wed, 04 May 2022',
+                meals: [
+                    {id: 1, meal_type: "meal", state: "active", name: "Spaggethi", allergies: [{id: 2, name: "Apple"}], kids: [2]},
+                    {id: 2, meal_type: "soup", state: "active", name: "Soup 1", allergies: [], kids: [1]}
+                ],
+            }
+        ])
     }
-
-    increment() {
-        this.state.value++;
-    }
+    static components = { LunchLine }
 }
-LunchMenu.template = "school_lunch.lunch_table"
+LunchMenuTable.template = "school_lunch.lunch_table"
+
+
+
 
 async function makeEnvironment() {
     const env = {};
@@ -46,7 +74,7 @@ async function makeEnvironment() {
 
 async function setup() {
     const env = await makeEnvironment();
-    mount(LunchMenu, {target: document.getElementById('LunchMenu'), env});
+    mount(LunchMenuTable, {target: document.getElementById('LunchMenu'), env});
 }
 
 whenReady(setup);
