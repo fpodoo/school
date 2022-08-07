1ea53a9d857bef7b7d76b1c46cb67c81fbcc5bd2
Fabien Pinckaers <fp@openerp.com>
2022-08-07 16:10:24 +0200

    fix

--- school_lunch/models/res_config_settings.py
+++ school_lunch/models/res_config_settings.py
@@ -28,6 +28,21 @@ class ResConfigSettings(models.TransientModel):
             related="company_id.lunch_reminder_template_id", string="Email Template", readonly=False,
             default=lambda self: self.env.ref('school_lunch.mail_template_school_lunch'))
 
+    def send_lunch_email(self):
+        self.env.company._cron_school_lunch_reminder()
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': _("Emails are scheduled to be sent in a few minutes"),
+                'type': 'warning',
+                'sticky': True,
+                'next': {'type': 'ir.actions.act_window_close'},
+            },
+        }
+
+        return {'warning': {'title': 'Warning', 'message': 'Skipping granting because order is under 1000.'}}
+
     def _get_lunch_reminder(self):
         cron = self.env.ref('school_lunch.school_menu_reminder')
         return cron.active and cron.nextcall.day or 0
