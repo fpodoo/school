f184ba7ad67e41ba4f3e9b58c39db3ffffc2dab9
Fabien Pinckaers <fp@openerp.com>
2022-08-09 21:49:33 +0200

    upate

--- school_lunch/__manifest__.py
+++ school_lunch/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.16',
+    'version': '0.17',
 
     # any module necessary for this one to work correctly
     'depends': [ 'website_sale'],
