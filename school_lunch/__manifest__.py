ff3a3a024c75282c9629c5e83edf1a003be38f51
Fabien Pinckaers <fp@odoo.com>
2022-08-30 21:09:07 +0200

    fix

--- school_lunch/__manifest__.py
+++ school_lunch/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.29',
+    'version': '0.30',
 
     # any module necessary for this one to work correctly
     'depends': ['website_sale', 'website_sale_gift_card'],
