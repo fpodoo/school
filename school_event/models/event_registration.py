1917209af3e1d453617fe364c275dec1b25edbed
Fabien Pinckaers <fp@odoo.com>
2022-09-18 21:23:49 +0200

    fix

---
+++ school_event/models/event_registration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# This module can be removed when upgrading to v16
+
+from odoo import fields, models
+
+class EventRegistrationAnswer(models.Model):
+    _inherit = 'event.registration.answer'
+    _rec_name = 'value_text_box'
+
+
