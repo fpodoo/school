713084932c3198dfa9d75d7503b54f543a11b576
Vincent Sevestre <100482501+vise-odoo@users.noreply.github.com>
2024-02-26 09:08:59 +0100

    [UPG][3764151] school_lunch: enable custom 're_config' view (#4)

--- school_lunch/migrations/17.0.1.0.0/pre-migrate.py
+++ school_lunch/migrations/17.0.1.0.0/pre-migrate.py
@@ -1,5 +1,6 @@
-from odoo.upgrade import util
+from odoo.upgrade import custom_util, util
 
 
 def migrate(cr, version):
     util.remove_module(cr, "school_event")
+    custom_util.activate_views(cr, "school_lunch.res_config_settings_school_lunch_form")
